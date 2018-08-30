cdb084a10306a32bf292e08f45a3cbe12eaa69a5
Louie Tan <louietyj@gmail.com>
2018-08-31 01:14:59 +0800

    Add /id method

--- turing_bot.py
+++ turing_bot.py
@@ -10,12 +10,22 @@ class TuringBot():
 
         # Add handlers
         self.dispatcher.add_handler(telegram.ext.CommandHandler('start', self.start_handler))
+        self.dispatcher.add_handler(telegram.ext.CommandHandler('id', self.id_handler))
         self.dispatcher.add_error_handler(self.error_handler)
         self.updater.start_polling()
 
+    @staticmethod
+    def _format_bot(text):
+        '''Format text that is sent from the bot and not from the other party'''
+        return f'[Bot] {text}'
+
     def start_handler(self, bot, update):
         chat_id = update.message.chat.id
-        bot.sendMessage(chat_id=chat_id, text='Hello world!')
+        bot.sendMessage(chat_id=chat_id, text=self._format_bot('Hello world!'))
+
+    def id_handler(self, bot, update):
+        chat_id = update.message.chat.id
+        bot.sendMessage(chat_id=chat_id, text=self._format_bot(str(chat_id)))
 
     def error_handler(self, bot, update, error):
         if isinstance(error, TimedOut):
