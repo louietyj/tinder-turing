2e018a1a72ccaebb52444739d9a1404c8bcb3260
Louie Tan <louietyj@gmail.com>
2018-09-03 04:27:34 +0800

    /name command

--- turing_bot.py
+++ turing_bot.py
@@ -1,6 +1,7 @@
 import telegram
 import telegram.ext
 import telegram.error
+from dal import *
 
 class TuringBot():
     def __init__(self, token):
@@ -10,6 +11,7 @@ class TuringBot():
 
         # Add handlers
         self.dispatcher.add_handler(telegram.ext.CommandHandler('start', self.start_handler))
+        self.dispatcher.add_handler(telegram.ext.CommandHandler('name', self.name_handler))
         self.dispatcher.add_handler(telegram.ext.CommandHandler('id', self.id_handler))
         self.dispatcher.add_error_handler(self.error_handler)
         self.updater.start_polling()
@@ -21,7 +23,26 @@ class TuringBot():
 
     def start_handler(self, bot, update):
         chat_id = update.message.chat.id
-        bot.sendMessage(chat_id=chat_id, text=self._format_bot('Hello world!'))
+        msg = 'Hi! Please register by typing /name {name}.'
+        bot.sendMessage(chat_id=chat_id, text=self._format_bot(msg))
+
+    def name_handler(self, bot, update):
+        chat_id = update.message.chat.id
+        try:
+            name = update.message.text.split(maxsplit=1)[1]
+            name = name.strip()
+            assert name.encode('ascii').decode() == name
+        except Exception:
+            msg = 'Please enter your name with only letters and numbers, e.g. no emojis.'
+            bot.sendMessage(chat_id=chat_id, text=self._format_bot(msg))
+            return
+        try:
+            db.users.replace_one({'tid': chat_id}, {'tid': chat_id, 'name': name}, True)
+        except Exception as e:
+            bot.sendMessage(chat_id=chat_id, text=self._format_bot(f'Error: {e}'))
+            return
+        msg = f'You have successfully registered as: {name}.'
+        bot.sendMessage(chat_id=chat_id, text=self._format_bot(msg))
 
     def id_handler(self, bot, update):
         chat_id = update.message.chat.id
