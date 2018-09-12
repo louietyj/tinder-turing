17daaac8b9d5acc09b8932b0b92bdcb07e797698
Louie Tan <louietyj@gmail.com>
2018-09-12 16:58:00 +0800

    Keep a reference to the DB connection
    So we can reset the DB

--- dal.py
+++ dal.py
@@ -43,4 +43,4 @@ def get_pair_by_tid(tid, is_active=True):
     return Pair.objects(Q(is_active=is_active) & (Q(tid1=str(tid)) | Q(tid2=str(tid))))
 
 # establish connection to mongodb instance
-connect('tinder_turing', host='localhost', port=27017)
+db = connect('tinder_turing', host='localhost', port=27017)
