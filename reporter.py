3d6c73dc5d7ee138602fc3d1d0e1a44166cd27e2
Darren Wee <darren.wee@u.nus.edu>
2018-09-15 02:07:11 +0800

    Implement pair ranking

--- reporter.py
+++ reporter.py
@@ -1,6 +1,7 @@
 from dal import *
 import textwrap
 import tabulate
+from operator import itemgetter
 
 BOT_NAME = 'Bot'
 
@@ -24,7 +25,7 @@ def _summarize_user(user):
 
 
 def get_pair_summary(is_active=True):
-    header = ['round', '', '', 'active?', 'start time']
+    header = ['round', 'p1', 'tid1', 'conf1', 'p2', 'tid2', 'conf2', 'active?', 'start time']
     summary = []
     for pair in Pair.objects(Q(is_active=is_active)):
         summary.append(_summarize_pair(pair))
@@ -33,6 +34,33 @@ def get_pair_summary(is_active=True):
 
 
 def _summarize_pair(pair):
+    name1 = get_name_by_tid(pair.tid1)
+    if pair.tid2 is None:
+        name2 = BOT_NAME
+    else:
+        name2 = get_name_by_tid(pair.tid2)
+    return [pair.round_num, name1, pair.tid1, pair.confidence1, name2, pair.tid2, pair.confidence2, pair.is_active, _get_pretty_timestamp(pair.start_time)]
+
+
+def get_ranking():
+    header = ['round', 'p1', 'tid1', 'conf1', 'p2', 'tid2', 'conf2', 'active?', 'start time']
+    # rank all human-bot pairs
+    bot_rank = []
+    for pair in Pair.objects(tid2=None).order_by('confidence1', 'confidence2'):
+        bot_rank.append(_summarize_pair(pair))
+    print('---- HUMAN-BOT PAIRS ----')
+    print(tabulate.tabulate(bot_rank, headers=header))
+
+    # rank all human-human pairs
+    human_rank = []
+    for pair in Pair.objects(tid2__ne=None).order_by('-confidence1', '-confidence2'):
+        human_rank.append(_summarize_pair(pair))
+    print('\n\n---- HUMAN-HUMAN PAIRS ----')
+    print(tabulate.tabulate(human_rank, headers=header))
+    return
+
+
+def _summarize_pair_with_conf(pair):
     name1 = get_name_by_tid(pair.tid1)
     if pair.tid2 is None:
         name2 = BOT_NAME
