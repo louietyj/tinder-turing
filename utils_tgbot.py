de5a1c254d59b61e5ef537dabce3e8f543353d75
Louie Tan <louietyj@gmail.com>
2018-09-15 23:17:58 +0800

    Add default timeout=20 to sendMessage
    Fixes #11

--- utils_tgbot.py
+++ utils_tgbot.py
@@ -4,7 +4,8 @@ import telegram
 import telegram.error
 import time
 
-RETRY_FUNCS = {'sendMessage'}
+RETRY_FUNCS = {'sendMessage', 'send_message'}
+TIMEOUT_FUNCS = {'sendMessage', 'send_message'}
 
 def try_repeat_wrapper(func, retries=100):
     @functools.wraps(func)
@@ -19,10 +20,22 @@ def try_repeat_wrapper(func, retries=100):
         raise error
     return wrapped_func
 
+def timeout_wrapper(func, timeout=20):
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs):
+        if 'timeout' not in kwargs:
+            kwargs['timeout'] = timeout
+        return func(*args, **kwargs)
+    return wrapped_func
+
 class BotWrapper:
     def __init__(self, bot):
         self._bot = bot
 
     def __getattr__(self, attr):
         _attr = getattr(self._bot, attr)
-        return try_repeat_wrapper(_attr) if attr in RETRY_FUNCS else _attr
+        if attr in RETRY_FUNCS:
+            _attr = try_repeat_wrapper(_attr)
+        if attr in TIMEOUT_FUNCS:
+            _attr = timeout_wrapper(_attr)
+        return _attr
